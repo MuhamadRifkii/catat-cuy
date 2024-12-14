7362235f1599d58dfdc611130e96f7c474c63805
MuhamadRifkii <mrifkibhae@gmail.com>
2024-12-14 14:00:47 +0700

    feat(search): add search filter

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -2,12 +2,10 @@
 import { useNavigate } from "react-router-dom";
 import Profile from "../Cards/Profile";
 import Searchbar from "../Searchbar/Searchbar";
-import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { resetForm } from "../../store/actions/login.action";
 
-export default function Navbar({ userInfo }) {
-  const [searchQuery, setSearchQuery] = useState("");
+export default function Navbar({ userInfo, setFilter, filterValue }) {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
@@ -18,25 +16,25 @@ export default function Navbar({ userInfo }) {
   };
 
   const handleSearch = () => {
-    // navigate(`/search?query=${searchQuery}`);
+    // Implement any specific search action if needed
   };
 
   const onClearSearch = () => {
-    setSearchQuery("");
+    setFilter(""); // Clear the search filter
   };
 
   return (
-    <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
+    <div className="sticky top-0 z-50 bg-white flex items-center justify-between px-6 py-2 drop-shadow">
       <h2 className="text-xl font-medium text-black py-2">Catat Cuy</h2>
 
-      <Searchbar
-        value={searchQuery}
-        onChange={({ target }) => {
-          setSearchQuery(target.value);
-        }}
-        handleSearch={handleSearch}
-        onClearSearch={onClearSearch}
-      />
+      {userInfo && (
+        <Searchbar
+          value={filterValue} // Controlled input for filter value
+          onChange={(e) => setFilter(e.target.value)} // Update the search query
+          handleSearch={handleSearch}
+          onClearSearch={onClearSearch}
+        />
+      )}
 
       <Profile userInfo={userInfo} logout={logout} />
     </div>
