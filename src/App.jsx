a0011fd3f60262ddd94b918f913615c1f456ecc0
MuhamadRifkii <mrifkibhae@gmail.com>
2024-12-15 22:15:11 +0700

    revert(config): revert vite config

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/SignUp/SignUp";
@@ -6,6 +11,7 @@ import SignUp from "./pages/SignUp/SignUp";
 const routes = (
   <Router>
     <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/login" exact element={<Login />} />
       <Route path="/signup" exact element={<SignUp />} />
       <Route path="/dashboard" exact element={<Home />} />
